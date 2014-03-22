847a9b9318d2630781f99e52ee73c344e0c27d99
Javran Cheng <Javran.c@gmail.com>
2014-03-22 19:23:10 -0400

    first working impl

--- src/Game/H2048/UI/Vty.hs
+++ src/Game/H2048/UI/Vty.hs
@@ -10,9 +10,55 @@ import Graphics.Vty
 -- import Graphics.Vty.LLInput
 import qualified Data.Text as T
 import Control.Monad
+import Control.Monad.Random
 -- import Control.Concurrent
 -- import Data.Monoid
 import Data.Foldable (foldMap)
+import Data.IORef
+import Data.Maybe
+
+import Game.H2048.Core
+
+data PlayState g = PlayState Board Int g
+
+-- pair: ({val}, ({row},{col}))
+toIndexedBoard :: Board -> [(Int, (Int, Int))]
+toIndexedBoard b = concat $ zipWith go [0..] taggedCols
+    where
+        -- board with each cell tagged with its col num
+        taggedCols = map (zip [0..]) b
+        go :: Int -> [(Int,Int)] -> [(Int,(Int,Int))]
+        go row = map (\(col,val) -> (val,(row,col)))
+
+renderGame :: PlayState g -> [[Widget FormattedText]] ->  IO ()
+renderGame (PlayState bd _ _) items = do
+    let ixBd = toIndexedBoard bd
+        renderCell v (row,col) = do
+            let item = items !! row !! col
+            item `setTextWithAttrs` [ ( (T.pack . show) v, Attr Default (SetTo cyan) Default )   ]
+
+    mapM_ (uncurry renderCell) ixBd
+
+newDirGameUpdate :: (RandomGen g)
+                 => IORef (PlayState g)
+                 -> [[Widget FormattedText]]
+                 ->  Dir
+                 -> IO Bool
+newDirGameUpdate psR items dir = do
+    (PlayState b1 s1 g1) <- readIORef psR
+    let updated = updateBoard dir b1
+        onSuccessUpdate (BoardUpdated b2 newS2) = do
+            -- TODO random.
+            (maybeB3,g2) <- runRandT (insertNewCell b2) g1
+            let b3 = fromMaybe b2 maybeB3
+                ps2 = PlayState b3 (s1 + newS2) g2
+            renderGame ps2 items
+            writeIORef psR ps2
+            return True
+    maybe
+        (return True)
+        onSuccessUpdate
+        updated
 
 mainVty :: IO ()
 mainVty = do
@@ -47,20 +93,31 @@ mainVty = do
     items  <- (replicateM 4 . replicateM 4) (plainText " ")
 
     mapM_ (addRow tbl . foldMap mkRow) items
-    (mapM_ . mapM_) (`setTextWithAttrs` [(" ", Attr Default (SetTo cyan) Default )]) items
 
-    -- some random cells for visualization
-    mapM_ (\(ind,x) ->
-               setTextWithAttrs x [( T.pack ( show (2 ^ (1+( ind * 37 `mod` 7) ) :: Int))
-                                   , Attr (SetTo bold) (SetTo (ISOColor ind)) Default)])
-         $ zip (filter (/=8) [1..]) $ concat items
+    -- prepare data and initialize
+    g <- newStdGen
+    ((bd,s),g') <- runRandT initGameBoard g
+    let ps = PlayState bd s g'
+    playStateR <- newIORef ps
+
+    renderGame ps items
 
     c <- newCollection
     _ <- addToCollection c ui fg
 
     pScore `onKeyPressed` \_ key _ ->
-        if key == KASCII 'q'
-            then shutdownUi >> return True
-            else return False
+        case key of
+          KASCII 'q' ->
+              shutdownUi >> return True
+          KASCII 'i' ->
+              newDirGameUpdate playStateR items DUp
+          KASCII 'k' ->
+              newDirGameUpdate playStateR items DDown
+          KASCII 'j' ->
+              newDirGameUpdate playStateR items DLeft
+          KASCII 'l' ->
+              newDirGameUpdate playStateR items DRight
+          _ ->
+              return False
 
     runUi c defaultContext
