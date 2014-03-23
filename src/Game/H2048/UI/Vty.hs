5ee94148ba73a565903c33441d7299976ec4b1a2
Javran Cheng <Javran.c@gmail.com>
2014-03-22 21:18:10 -0400

    comments on vty-ui impl

--- src/Game/H2048/UI/Vty.hs
+++ src/Game/H2048/UI/Vty.hs
@@ -16,16 +16,17 @@ import Data.Maybe
 
 import Game.H2048.Core
 
+-- | indicate the status of a playing session
 data PlayState g = PlayState
-    { psBoard  :: Board
-    , psScore  :: Int
-    , psGState :: GameState
-    , psRGen   ::  g
+    { psBoard  :: Board     -- ^ current board
+    , psScore  :: Int       -- ^ current collected score
+    , psGState :: GameState -- ^ indicate whether the game terminates
+    , psRGen   ::  g        -- ^ next random generator
     }
 
--- TODO documentation!
-
--- pair: ({val}, ({row},{col}))
+-- | flatten a 2D list, and keep the original
+--   coordinate with the actual value, each element
+--   in the resulting list looks like @({value},({row index},{colum index}))@.
 toIndexedBoard :: Board -> [(Int, (Int, Int))]
 toIndexedBoard b = concat $ zipWith go [0..] taggedCols
     where
@@ -34,6 +35,7 @@ toIndexedBoard b = concat $ zipWith go [0..] taggedCols
         go :: Int -> [(Int,Int)] -> [(Int,(Int,Int))]
         go row = map (\(col,val) -> (val,(row,col)))
 
+-- | calculate colors and styles for a given number
 colorize :: Int -> [(T.Text, Attr)]
 colorize i = [(s,attr)]
     where
@@ -55,40 +57,51 @@ colorize i = [(s,attr)]
             , (2048, ( bold, ISOColor 3))
             ]
 
-renderGame :: PlayState g
-           -> [[Widget FormattedText]]
-           -> Widget FormattedText
+-- | render UI according to the PlayState
+renderGame :: PlayState g              -- ^ the PlayState
+           -> [[Widget FormattedText]] -- ^ cell widgets
+           -> Widget FormattedText     -- ^ status bar widget
            ->  IO ()
 renderGame (PlayState bd sc gs _) items st = do
     let ixBd = toIndexedBoard bd
         renderCell v (row,col) = do
             let item = items !! row !! col
+            -- for each cell, colorize it
+            -- and update the corresponding widget
             item `setTextWithAttrs` colorize v
+        -- the beginning of status bar
         scoreDesc = case gs of
                       Win -> "You win. Final Score: "
                       Lose -> "Game Over. Final Score: "
                       Alive -> "Current Score: "
 
+    -- update table
     mapM_ (uncurry renderCell) ixBd
+    -- update status bar
     setText st $ T.pack (scoreDesc ++ show sc)
 
+-- | perform game update when a new direction is given
 newDirGameUpdate :: (RandomGen g)
-                 => IORef (PlayState g)
-                 -> [[Widget FormattedText]]
-                 -> Widget FormattedText
-                 ->  Dir
+                 => IORef (PlayState g)      -- ^ where PlayState is held
+                 -> [[Widget FormattedText]] -- ^ cell widgets
+                 -> Widget FormattedText     -- ^ status bar
+                 -> Dir                      -- ^ new direction
                  -> IO Bool
 newDirGameUpdate psR items st dir = do
     (PlayState b1 s1 gs1 g1) <- readIORef psR
     let updated = updateBoard dir b1
         onSuccessUpdate (BoardUpdated b2 newS2) = do
-            -- if we are still alive, this insertion is always possible
+            -- if we are still alive, this insertion is always possible,
+            -- because a successful update means at least two cells
+            -- are merged together, leaving at least one empty cell
             (Just b3,g2) <- runRandT (insertNewCell b2) g1
+            -- create new PlayState, collect score, sync GameState
             let ps2 = PlayState b3 (s1 + newS2) (gameState b3) g2
             renderGame ps2 items st
+            -- update PlayState to the IORef
             writeIORef psR ps2
             return True
-    -- only update if alive
+    -- 1. only update if alive
     case gs1 of
       Win ->
           return True
@@ -96,10 +109,14 @@ newDirGameUpdate psR items st dir = do
           return True
       Alive ->
           maybe
+             -- 2(a). update failed, invalid move, do nothing
              (return True)
+             -- 2(b). updated successfully
              onSuccessUpdate
+             -- 2. try to update the board
              updated
 
+-- | the entry for vty-ui CLI implementation
 mainVty :: IO ()
 mainVty = do
     let cellSample :: String
@@ -116,11 +133,9 @@ mainVty = do
     tbl <- newTable (replicate 4 cellSpec) BorderFull
 
     pScore <- plainText " <Score> "
-    pHelp  <- plainText   helpString
+    pHelp  <- plainText helpString
     hints  <- hCentered pScore <--> hCentered pHelp
-
     allW   <- hCentered tbl <--> return hints
-
     ui     <- centered allW
 
     fg <- newFocusGroup
@@ -136,15 +151,19 @@ mainVty = do
     g <- newStdGen
     ((bd,s),g') <- runRandT initGameBoard g
 
+    -- keep track of playing state using IORef
     let ps = PlayState bd s Alive g'
     playStateR <- newIORef ps
 
+    -- first rendering
     renderGame ps items pScore
 
     c <- newCollection
     _ <- addToCollection c ui fg
 
+    -- shorthand for event update
     let doUpdate = newDirGameUpdate playStateR items pScore
+
     pScore `onKeyPressed` \_ key _ ->
         case key of
           KASCII 'q' ->
