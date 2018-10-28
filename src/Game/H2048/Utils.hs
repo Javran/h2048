4d430066937ff9eb33f2e276987862b7359d29b1
Javran Cheng <Javran.c@gmail.com>
2018-10-27 23:13:17 -0400

    remove vty-ui for now

--- src/Game/H2048/Utils.hs
+++ src/Game/H2048/Utils.hs
@@ -15,6 +15,8 @@ module Game.H2048.Utils
     )
 where
 
+import Control.Lens hiding (universe)
+
 -- provide utilities
 
 -- | all possible values for a Bounded Enum
@@ -27,12 +29,4 @@ inPos :: Int      -- ^ the index
       -> (a -> a) -- ^ a function from the old element to the new one
       -> [a]      -- ^ the list to be modified
       -> [a]
-inPos n f xs
-      | null xs    = xs
-      | n < 0      = xs
-      -- for all the cases below,
-      -- safely assume n >= 0 and xs is not empty
-      | n == 0     = let (y:ys) = xs
-                     in f y : ys
-      | otherwise  = let (y:ys) = xs
-                     in y : inPos (n - 1) f ys
+inPos n = over (ix n)
