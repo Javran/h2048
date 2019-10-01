82f1b14ecac3c9855e8f139acaadab3aada4fc9f
Javran Cheng <Javran.c@gmail.com>
2019-09-30 22:24:30 -0700

    impl rendering against a sample board.

--- src/Game/H2048/UI/Brick.hs
+++ src/Game/H2048/UI/Brick.hs
@@ -4,30 +4,48 @@ import Brick
 import Brick.Widgets.Border
 import Brick.Widgets.Center
 import Data.List
+import Data.Functor
 import Graphics.Vty.Attributes (defAttr)
 import Graphics.Vty.Input.Events
 
+import Game.H2048.Core
+
 data RName = RBoard deriving (Eq, Ord)
 
-boardWidget :: () -> Widget RName
-boardWidget _ =
+boardSample :: Board
+boardSample = mkBoard
+  [ [1,2,4,8]
+  , [16,32,64,128]
+  , [256,512,1024,2048]
+  , [0,0,0,0]
+  ]
+
+boardWidget :: Board -> Widget RName
+boardWidget bdOpaque =
     center
     . joinBorders
     . border
     $ grid
   where
+    bd = fromBoard bdOpaque
     grid =
-      hLimit (hMax*4+3) $ vBox (intersperse hBorder (replicate 4 row))
-    row =
+      hLimit (hMax*4+3) $ vBox (intersperse hBorder (row <$> [0..3]))
+    row :: Int -> Widget RName
+    row r =
       vLimit 1 $
-        hBox (intersperse vBorder (replicate 4 sampleCell))
+        hBox (intersperse vBorder (cell r <$> [0..3]))
     contentSample = " 2048 "
     hMax = length contentSample
-    sampleCell =
-      vLimit 1
-      . hLimit hMax
-      . center
-      $ str contentSample
+    cell :: Int -> Int -> Widget RName
+    cell r c =
+      vLimit 1 . hLimit hMax $ cellW
+      where
+        val = bd !! r !! c
+        cellW =
+          if val == 0
+            then fill ' '
+            else padLeft Max
+              $ str (show (bd !! r !! c) <> " ")
 
 main :: IO ()
 main = do
@@ -39,5 +57,5 @@ main = do
         , appAttrMap = const $ attrMap defAttr []
         , appChooseCursor = neverShowCursor
         }
-      initState = ()
-  defaultMain app initState
+      initState = boardSample
+  void $ defaultMain app initState
