f81c8b891f1f6be52b0f4cc857c97b7e2bf70270
Javran Cheng <Javran.c@gmail.com>
2019-10-03 22:48:04 -0700

    some initial stuff.

--- src/Game/H2048/NewCore.hs
+++ src/Game/H2048/NewCore.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE MonadComprehensions #-}
 module Game.H2048.NewCore where
+
+import qualified Data.Map.Strict as M
 
 {-
   TODO: this is an overhaul of Game.H2048.Core .. hmm just for fun.
@@ -14,7 +17,23 @@ module Game.H2048.NewCore where
     For example, rules like the following is kind of arbitrary and one should
     be able to change them without having to modify core:
 
+    - size of the board is 4x4.
     - 90% time we get a 2, otherwise 4.
     - get a 2048 to be considered having won.
+    - how many score you get by merging.
 
  -}
+
+newtype Cell = Cell { _cTier :: Int }
+
+merge :: Cell -> Cell -> Maybe Cell
+merge (Cell a) (Cell b) = [ Cell (succ a) | a == b ]
+
+type Coord = (Int, Int) -- (<row>, <col>) 0-based.
+
+data Board
+  = Board
+    { _bdRows :: Int
+    , _bdCols :: Int
+    , _bdCells :: M.Map Coord Cell
+    }
