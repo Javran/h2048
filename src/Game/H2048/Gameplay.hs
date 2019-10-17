9d5895fd663c38f8c214621c21db54f23b49d17e
Javran Cheng <Javran.c@gmail.com>
2019-10-16 23:14:58 -0700

    recover spawnNewCell.

--- src/Game/H2048/Gameplay.hs
+++ src/Game/H2048/Gameplay.hs
@@ -19,9 +19,28 @@ data GameState
 data Gameplay
   = Gameplay
   { _gpRule :: GameRule -- game rules, must be read-only after creation.
-  , _gpState :: GameState -- the only mutating bit of gameplay.
+  , _gpScore :: Int
+  , _gpBoard :: GameBoard
+  , _gpGen :: TFGen
   }
 
 todo :: a
 todo = error "TODO"
 
+randomOp :: (TFGen -> (a, TFGen)) -> Gameplay -> (a, Gameplay)
+randomOp op gp = (v, gp { _gpGen = g' })
+  where
+    g = _gpGen gp
+    (v, g') = op g
+
+spawnNewCell :: Gameplay -> S.Set Coord -> Maybe (((Coord, Cell), S.Set Coord), Gameplay)
+spawnNewCell gp emptyCells = do
+  False <- pure $ S.null emptyCells
+  let -- step 1: pick an empty cell.
+      lowHigh = (0, S.size emptyCells - 1)
+      (i, gp') = randomOp (randomR lowHigh) gp
+      v = S.toAscList emptyCells !! i
+      -- step 2: pick a tier.
+      distrib = _grNewCellDistrib . _gpRule $ gp'
+      (tier, gp'') = randomOp (randomPick distrib) gp'
+  pure (((v, Cell tier), S.delete v emptyCells), gp'')
