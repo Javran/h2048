99724536b943154b094e35a14f1d6ef86574e97f
Javran Cheng <Javran.c@gmail.com>
2019-10-16 22:52:29 -0700

    scrap Gameplay design.
    Scrapping current design because I realized brick expect
    clients to provide a single state, not a monad transformer to work with.
    We could transform EventM but eventually we'll still need to figure out
    how to do the wrapping / unwrapping, might as well switch to a design
    that a single data type holds everything for us.

--- src/Game/H2048/Gameplay.hs
+++ src/Game/H2048/Gameplay.hs
@@ -9,9 +9,6 @@ import qualified Data.Set as S
 
 import Game.H2048.NewCore
 
-type GameplayT = RWST GameRule () GameState
-type Gameplay = GameplayT IO
-
 data GameState
   = GameState
     { _gsScore :: Int
@@ -19,77 +16,12 @@ data GameState
     , _gsGen :: TFGen
     }
 
+data Gameplay
+  = Gameplay
+  { _gpRule :: GameRule -- game rules, must be read-only after creation.
+  , _gpState :: GameState -- the only mutating bit of gameplay.
+  }
+
 todo :: a
 todo = error "TODO"
 
-gameRandomR :: (Monad m, Random a) => (a, a) -> GameplayT m a
-gameRandomR loHi = do
-  g <- gets _gsGen
-  let (v, g') = randomR loHi g
-  v <$ modify (\s -> s { _gsGen = g'})
-
-spawnNewCell :: Monad m => S.Set Coord -> GameplayT m (Maybe ((Coord, Cell), S.Set Coord))
-spawnNewCell emptyCells =
-  if S.null emptyCells
-    then pure Nothing
-    else do
-      i <- gameRandomR (0, S.size emptyCells - 1)
-      distrib <- asks _grNewCellDistrib
-      let v = S.toAscList emptyCells !! i
-      g <- gets _gsGen
-      let (tier, g') = randomPick distrib g
-      modify (\s -> s { _gsGen = g' })
-      pure $ Just ((v, Cell tier), S.delete v emptyCells)
-
--- this function should only fail when there is insufficient space
--- to fill an empty board with # of initial cell spawn specified in GameRule.
-newGame :: Monad m => GameplayT m ()
-newGame = do
-  coords <- asks allCoords
-  initSpawn <- asks _grInitSpawn
-  newBoard <-
-    fix (\loop curBoard needSpawn emptyCells ->
-           if needSpawn == 0
-             then pure curBoard
-             else do
-               m <- spawnNewCell emptyCells
-               case m of
-                 Just ((coord, cell), emptyCells') ->
-                   let curBoard' = M.insert coord cell curBoard
-                   in loop curBoard' (needSpawn - 1) emptyCells'
-                 Nothing ->
-                   error "Failed to create new game, no more space for empty cells."
-        )
-      M.empty
-      initSpawn
-      (S.fromDistinctAscList coords)
-  modify (\s -> s { _gsScore = 0, _gsBoard = newBoard } )
-
-{-
-
-  Try to apply a move on current state of the game, returns:
-
-  - `Nothing` if this move is invalid
-  - `Just moves` if this move is valid, also returns all possible moves
-    after the board is fully updated (meaning new cell has been spawned).
- -}
-stepGame :: Monad m => Dir -> GameplayT m (Maybe [Dir])
-stepGame dir = do
-  gr <- ask
-  coords <- asks allCoords
-  bd <- gets _gsBoard
-  case applyMove gr dir bd of
-    Nothing -> pure Nothing
-    Just (bd', award) -> do
-      modify (\s -> s { _gsScore = _gsScore s + award })
-      -- if a move can be applied successfully, that means
-      -- there must be empty cell on the board,
-      -- in other words, spawnNewCell should not fail.
-      let emptyCoords = S.filter (`M.notMember` bd') (S.fromDistinctAscList coords)
-      mResult <- spawnNewCell emptyCoords
-      case mResult of
-        Nothing -> error "Failed to spawn new cell, this should not happen."
-        Just ((coord, cell), _) -> do
-          let bd'' = M.insert coord cell bd'
-          modify (\s -> s { _gsBoard = bd'' })
-          pure $ Just (possibleMoves gr bd'')
